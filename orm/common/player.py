9d999cbf9968d47529948f868132325d13ff81cd
Kevin Lau <laudevelopment@gmail.com>
2018-12-05 21:01:34 -0800

    Add audit columns and separate  into two files, player and player_url

--- orm/common/player.py
+++ orm/common/player.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import Column, BigInteger, Integer, String, Date
+from sqlalchemy import (Column, BigInteger, Integer, String, Date,
+    DateTime)
 
 Base = declarative_base()
 
@@ -17,17 +18,10 @@ class Player(Base):
     birthday = Column(Date)
     birthplace = Column(String(100))
     nationality = Column(String(50))
+    created_by = Column(String(255))
+    creation_date = Column(DateTime)
+    last_updated_by = Column(String(255))
+    last_updated_date = Column(DateTime)
 
     def __repr__(self):
         return "<Player(name='{name}')>".format(name = self.name)
-
-class PlayerUrl(Base):
-    __tablename__ = 'player_urls'
-    __table_args__ = {'schema':'fantalytix'}
-
-    player_id = Column(BigInteger, nullable=False)
-    url = Column(String(512), primary_key=True, nullable=False)
-    site = Column(String(50), nullable=False)
-
-    def __repr__(self):
-        return "<PlayerUrl(url='{url}')>".format(url=self.url[:10])
--- test/common/player_url.py
+++ test/common/player_url.py
@@ -1,36 +1,33 @@
 import unittest
 
-from datetime import date, datetime, timezone
+from datetime import datetime, timezone
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from orm.common.player import Player
+from orm.common.player_url import PlayerUrl
 from test.settings import CONNECTION
 
-class TestPlayerORM(unittest.TestCase):
+class TestPlayerUrlORM(unittest.TestCase):
     
     def setUp(self):
         self.engine = create_engine(CONNECTION)
         Session = sessionmaker(bind=self.engine)
         self.session = Session()
 
-    def test_player(self):
-        LBJ = Player(
-            name='LeBron James',
-            height = '6-8',
-            weight = 250,
-            birthday = date(1984, 12, 30),
-            birthplace = 'in Akron, Ohio',
-            nationality = 'us',
+    def test_player_url(self):
+        LBJ_URL = PlayerUrl(
+            player_id=1,
+            url='https://www.basketball-reference.com/players/j/jamesle01.html', 
+            site='basketball-reference',
             created_by='pycrawl',
             creation_date=datetime(2018, 1, 1, 0, 0, 0, tzinfo=timezone.utc),
             last_updated_by='pycrawl',
             last_updated_date=datetime(2018, 2, 2, 0, 0, 0, tzinfo=timezone.utc)
         )
-        self.session.add(LBJ)
-        test_player = self.session.query(Player).filter_by(name='LeBron James')
-        self.assertTrue(LBJ is test_player.first())
+        self.session.add(LBJ_URL)
+        test_player_url = self.session.query(PlayerUrl).filter_by(url='https://www.basketball-reference.com/players/j/jamesle01.html')
+        self.assertTrue(LBJ_URL is test_player_url.first())
 
 if __name__ == '__main__':
     unittest.main()
